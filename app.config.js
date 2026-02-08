fc2133d147c7a93f58973d15106630a6741d85ab
Pavan-Kumar-2095 <developer0exe@gmail.com>
2026-02-08 15:03:45 +0530

    commit

--- app.config.js
+++ app.config.js
@@ -1,33 +1,18 @@
-import 'dotenv/config';
-
 export default {
   expo: {
     name: "Raaga",
     slug: "mobileapp",
     version: "1.0.0",
-    sdkVersion: "54.0.0",
-    platforms: ["ios", "android", "web"],
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     scheme: "mobileapp",
     userInterfaceStyle: "automatic",
 
-    ios: {
-      bundleIdentifier: "com.yourname.raaga.dev", // unique for dev/testing
-      supportsTablet: true,
-      infoPlist: {
-        UIBackgroundModes: ["audio"] // background music
-      }
-    },
-
+    ios: { supportsTablet: true, infoPlist: { UIBackgroundModes: ["audio"] } },
     android: {
-      package: "com.yourname.raaga.dev", // unique for dev/testing
+      package: "com.anonymous.mobileapp",
       versionCode: 1,
-      permissions: ["FOREGROUND_SERVICE"], // background music
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/logo.png",
-        backgroundColor: "#E6F4FE"
-      }
+      adaptiveIcon: { foregroundImage: "./assets/images/logo.png", backgroundColor: "#E6F4FE" }
     },
 
     plugins: [
@@ -45,11 +30,12 @@ export default {
     experiments: { typedRoutes: true },
 
     extra: {
+      router: {},
       eas: {
-        projectId: process.env.EXPO_PUBLIC_EAS_PROJECT_ID
+        projectId: "89628e76-6a53-4519-a81d-5377763fac76"
       },
-      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
+      supabaseUrl: "https://pqsodsdarprppbvesfel.supabase.co",
+      supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBxc29kc2RhcnBycHBidmVzZmVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NTYzMDUsImV4cCI6MjA4NTMzMjMwNX0.veXsW-48V44QOXw2AJ8_O5zNrNXIundIPGbe4zM96ts"
     }
   }
 };
